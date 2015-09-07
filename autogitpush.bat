999d51e25fe098186ba797451659f96773fcc5ee
ramonfe <ramonfe@gmail.com>
2015-09-07 14:15:03 -0700

    Auto-committed on Mon 09/07/2015

--- autogitpush.bat
+++ autogitpush.bat
@@ -2,7 +2,6 @@
 cd "c:\myprojects\github\casadecambio"
 set GIT_PATH="C:\MyProjects\PortableGit\bin\git.exe"
 
-
   %GIT_PATH% add -A
 	%GIT_PATH% commit -am "Auto-committed on %date%"
 	%GIT_PATH% push
