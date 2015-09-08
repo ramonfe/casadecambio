80fc8983991a0e9e0280233a89dbb4405b5dad38
unknown <jigs@eeappserver.foxconn-bc.com>
2015-09-08 12:06:02 -0700

    Auto-committed on Tue 09/08/2015

--- autogitpush.bat
+++ autogitpush.bat
@@ -1,6 +1,6 @@
 @echo off
-cd "c:\myprojects\github\casadecambio"
-set GIT_PATH="C:\MyProjects\PortableGit\bin\git.exe"
+cd "c:\software\github\casadecambio"
+set GIT_PATH="C:\software\github\PortableGit\bin\git.exe"
 
   %GIT_PATH% add -A
 	%GIT_PATH% commit -am "Auto-committed on %date%"
